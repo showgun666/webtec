c4b47490737c2a7c6c7a43e461087ac93fa29dbe
Markus Lindgren <mavn22@bth.se>
2023-02-19 05:26:39 +0100

    added rainbow class and today's day

--- kmom01/public/today.php
+++ kmom01/public/today.php
@@ -10,8 +10,13 @@ date_default_timezone_set('Europe/Stockholm');
 // The time of now
 $time = date('H:i:s');
 
+// Name of the week day
+$weekday = date('l');
+
 ?>
 <main>
+    <p>Idag är det <span class="rainbow"><?= $weekday ?></span>.</p>
+    
     <p>Nu är klockan <?= $time ?> i Sverige.</p>
 </main>
 
