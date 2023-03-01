9b353c51523288d7e5702d07e926ab979810d4ec
Markus Lindgren <mavn22@bth.se>
2023-03-01 13:39:09 +0100

    first commit

---
+++ report/public/play.php
@@ -0,0 +1,23 @@
+<?php
+include('../config/config.php');
+
+$title = 'Träna på PHP';
+include('../view/header.php');
+include('../view/navbar.php');
+?>
+
+<div class="two-col-layout">
+    <main Class="main">
+        <h1><?= $title ?></h1>
+        <p>Här bygger vi kod för att träna på PHP och dess olika konstruktioner som skall hjälpa oss att generera webbsidor.</p>
+
+        <?php include('../view/php/hello.php') ?>
+        
+    </main>
+
+    <aside class="aside">
+        <p>Här får vi lista ut lite saker som vi ska ha.</p>
+    </aside>
+</div>
+
+<?php include('../view/footer.php') ?>
