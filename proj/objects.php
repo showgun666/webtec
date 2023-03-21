c21e61f2c995cd2e2e8f9cc1b20fc3551aab2d1f
Markus Lindgren <mavn22@bth.se>
2023-03-21 09:35:39 +0100

    first commit

---
+++ proj/objects.php
@@ -0,0 +1,10 @@
+<?php
+include('../config/config.php');
+
+$title = 'objekt';
+include('../view/header.php');
+include('../view/side.php');
+?>
+
+<?php
+include('../view/footer.php');
---
+++ proj/presentation.php
@@ -0,0 +1,16 @@
+<?php
+include('../config/config.php');
+
+$title = 'presentation';
+include('../view/header.php');
+include('../view/side.php');
+?>
+
+<div class="content-box content-right text-center">
+    <h3>Webbsidans Skapare</h3>
+    <p>Den här webbsidan skapades som slutprojekt i kursen webbteknologier på Blekinge Tekniska högskola av Markus von Wowern.</p>
+    <p>Det är den första webbsidan som Markus skapat på egen hand och visar på vad han har lärt sig under kursens gång.</p>
+    <p>Webbplatsen använder php, css och hämtar information från en databas för att visa upp.</p>
+</div>
+<?php
+include('../view/footer.php');
