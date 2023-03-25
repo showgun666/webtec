73e7f75d5c772f9d86bdd3e374db12ff928aa29d
Markus Lindgren <mavn22@bth.se>
2023-03-25 01:28:00 +0100

    adding object preview

--- proj/public/home.php
+++ proj/public/home.php
@@ -9,18 +9,9 @@ include('../view/side.php');
     <div class="content-box content-left text-center">
         <h3 class="content-title">Välkommen till Begravningsmuseum Online</h3>
         <p>Begravningsmuseum Online(BMO) är en hemsida vars syfte är att visa upp och vårda ett kulturarv.</p>
+        <p>Överst på sidan så kan du navigera till de olika objekt och artiklar som vi har.</p>
     </div>
     <div class="content-box content-right text-right">
-        <h3>Tillgänglighet</h3>
-        <p>Begravningsmuseum Online(BMO) arbetar med för att alla som har tillgång till internet ska kunna ta del av våra utställningar och allt bedrivs ideellt för stora som små.</p>
-    </div>
-    <div class="content-box content-left text-left">
-        <h3>Tillgänglighet</h3>
-        <p>Begravningsmuseum Online(BMO) arbetar med för att alla som har tillgång till internet ska kunna ta del av våra utställningar och allt bedrivs ideellt för stora som små.</p>
-    </div><div class="content-box content-right text-right">
-        <h3>Tillgänglighet</h3>
-        <p>Begravningsmuseum Online(BMO) arbetar med för att alla som har tillgång till internet ska kunna ta del av våra utställningar och allt bedrivs ideellt för stora som små.</p>
-    </div><div class="content-box content-left text-left">
         <h3>Tillgänglighet</h3>
         <p>Begravningsmuseum Online(BMO) arbetar med för att alla som har tillgång till internet ska kunna ta del av våra utställningar och allt bedrivs ideellt för stora som små.</p>
     </div>
